d7604534401df9275f56a50925b81336fd596ccb
Daniel Hilst Selli <danielhilst@gmail.com>
2018-09-28 01:09:25 -0300

    Filter transactions by user

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 	gem 'pry'
+	gem 'sqlite3'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_023110) do
+ActiveRecord::Schema.define(version: 2018_09_28_031613) do
 
   create_table "statements", force: :cascade do |t|
     t.datetime "created_at", null: false
