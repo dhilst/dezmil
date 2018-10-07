bb8c12184faa358835ae78d93028746da40f101c
Daniel Hilst Selli <danielhilst@gmail.com>
2018-10-07 19:28:47 -0300

    Separate debit and credit

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,17 @@
 class Transaction < ApplicationRecord
   belongs_to :statement
   validates :memo, uniqueness: { scope: %i[date amount] }
-  scope :month, -> (d) { where(date: d.beginning_of_month..d.end_of_month) }
+  scope :month, -> (d) { where('transactions.date' => d.beginning_of_month..d.end_of_month) }
   belongs_to :category, optional: true
   scope :agrep, -> (pattern, ratio) {
     where('length(memo) - levenshtein(lower(memo),lower(:pattern)) > length(:pattern) * :ratio', pattern: pattern, ratio: ratio)
   }
+
+  def debit
+    amount <= 0 ? amount : nil
+  end
+
+  def credit
+    amount > 0 ? amount : nil
+  end
 end
