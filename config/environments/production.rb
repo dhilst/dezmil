28ea27e7f7ab6c08c8cf67fc7b3e7aa376a39d4c
dhilst <danielhilst@gmail.com>
2018-10-12 21:28:02 -0300

    Fix quotes on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "https://sheltered-atoll-35411.herokuapp.com" }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-   address: ‘smtp.sendgrid.net’,
+   address: 'smtp.sendgrid.net',
    port: “25”,
    domain: ‘heroku.com’,
    user_name: ENV[“SENDGRID_USERNAME”],
