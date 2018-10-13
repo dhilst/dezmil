e5c88348f69448e2b6bb5f22c619a666d10ca4bb
dhilst <danielhilst@gmail.com>
2018-10-12 21:30:05 -0300

    Fix quotes on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,11 +95,11 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
    address: 'smtp.sendgrid.net',
-   port: “25”,
-   domain: ‘heroku.com’,
-   user_name: ENV[“SENDGRID_USERNAME”],
-   password: ENV[“SENDGRID_PASSWORD”],
-   authentication: ‘plain’,
+   port: "25",
+   domain: 'heroku.com',
+   user_name: ENV["SENDGRID_USERNAME"],
+   password: ENV["SENDGRID_PASSWORD"],
+   authentication: 'plain',
    enable_starttls_auto: true
   }
 end
