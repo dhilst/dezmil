d64bd85965c81ca3b340b4e7bd27fc3d479971a4
dhilst <danielhilst@gmail.com>
2018-10-14 20:13:01 -0300

    Fix url on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => "https://sheltered-atoll-35411.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "https://dezmil.herokuapp.com" }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
    address: 'smtp.sendgrid.net',
