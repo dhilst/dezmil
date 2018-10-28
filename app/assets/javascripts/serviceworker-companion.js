892ed63914df3496ab623777e969f5befcff5294
dhilst <danielhilst@gmail.com>
2018-10-28 10:56:42 -0300

    Fix service worker

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -2,12 +2,13 @@ if (navigator.serviceWorker) {
   navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
     .then(async function(reg) {
       console.log('[Companion]', 'Service worker registered!', reg.update());
-
       try {
         const update = await reg.update();
         console.log('service worker updated')
       } catch (error) {
         console.erro('service worker update error', error);
       }
+    }).catch(function(error) {
+      console.error('Service worker registration error', error);
     });
 }
