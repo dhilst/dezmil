419e1301beb5439e1f0fdcc3418ae43d1ce8b33b
dhilst <danielhilst@gmail.com>
2018-11-30 20:34:53 -0200

    Add zoom

--- app/javascript/packs/transactions.js
+++ app/javascript/packs/transactions.js
@@ -5,7 +5,7 @@ class PageDate {
 	constructor() {
 		if (location.pathname.match(/transactions\/(\d{4})\/(\d{1,2})/)) {
 			var dummy;
-			[dummy, this.year, this.month] = location.pathname.match(/transactions\/(\d{4})\/(\d{1,2})/);
+      [dummy, this.year, this.month, this.rest] = location.pathname.match(/transactions\/(\d{4})\/(\d{1,2})\/?(.*)/);
 		} else {
 			this.date = new Date();
 			this.year = now.getFullYear();
@@ -44,12 +44,10 @@ const ready = async () => {
       .append('g')
       .attr('transform', 'translate(0, 100)')
 
-
   const x = d3.scaleLinear().domain([1,31]).range([margin + mi, width - margin - mi])
   const y = d3.scaleLinear().domain([yMax, yMin]).range([margin, height - margin - mi]);
   const xAxis = d3.axisBottom(x).ticks(8)
   const yAxis = d3.axisLeft(y).ticks(5)
-
 
   const circles = svg
     .selectAll('circle')
@@ -57,17 +55,32 @@ const ready = async () => {
     .enter().append('circle')
       .attr('fill', d => d.color)
       .attr('stroke', d => 'black')
-      .attr('r', d => 4)
+      .attr('r', d =>   2)
       .attr('cx', d => x(d.date.getDate()))
       .attr('cy', d => y(d.amount))
 
-  svg.append('g')
+  const gX = svg.append('g')
     .attr('transform', `translate(0, ${height - margin + 5})`)
     .call(xAxis)
 
-  svg.append('g')
+  const gY = svg.append('g')
     .attr('transform', `translate(${margin}, 0)`)
     .call(yAxis)
+
+  function zoomFunction(transform){
+    // create new scale ojects based on event
+    var new_xScale = d3.event.transform.rescaleX(x)
+    var new_yScale = d3.event.transform.rescaleY(y)
+    gX.call(xAxis.scale(new_xScale));
+    gY.call(yAxis.scale(new_yScale));
+
+    // update circle
+    circles.attr("transform", d3.event.transform).attr('r', d => 4/transform.k)
+  };
+
+  const zoom = d3.zoom()
+      .on("zoom", zoomFunction);
+  svg.call(zoom)
 
   svg.append('text')
     .attr('transform', `translate(${margin}, ${height - margin + 30})`)
@@ -89,6 +102,7 @@ const ready = async () => {
       .attr('x', x)
       .attr('transform', `rotate(270 ${x} ${y})`)
   }
+
 
 }
 
